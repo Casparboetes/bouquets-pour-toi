e8cd65b03518fef949e313ab36850c86ada1ae84
Casparboetes <34174855+Casparboetes@users.noreply.github.com>
2023-12-18 09:16:22 +0100

    feat: moves logic to page to keep the list as dumb as possible.

--- app/components/FlowerList.tsx
+++ app/components/FlowerList.tsx
@@ -1,40 +1,17 @@
-'use client';
-
 import ProductCard from '@/app/components/ProductCard';
-import { useEffect, useState } from 'react';
-import { GetBouquets } from '@/app/types/products.models';
+import type { GetBouquets } from '@/app/types/products.models';
 import Toggle from '@/app/components/Toggle';
 import { useToggle } from '@uidotdev/usehooks';
 
-const FlowerList = () => {
-    const [allProducts, setAllProducts] = useState<GetBouquets | null>(null);
-    const [on, toggle] = useToggle(true);
-    const fetchProducts = async () => {
-        const response = await fetch('http://localhost:3000/api/products');
-        const data = await response.json();
-
-        setAllProducts(data);
-    };
-
-    useEffect(() => {
-        fetch('http://localhost:3000/api/products', {
-            method: 'PUT',
-            headers: new Headers({
-                'Content-Type': 'application/json',
-                Accept: 'application/json',
-            }),
-            body: JSON.stringify({ priceIncludingVat: on }),
-        })
-            .then((response) => response.json())
-            .then((data) => setAllProducts(data))
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-    }, [on]);
-
-    useEffect(() => {
-        fetchProducts();
-    }, []);
+const FlowerList = ({
+    products,
+    on,
+    toggle,
+}: {
+    products: GetBouquets | null;
+    on: boolean;
+    toggle: () => void;
+}) => {
 
     return (
         <>
@@ -45,8 +22,8 @@ const FlowerList = () => {
                 <Toggle on={on} toggle={toggle} />
             </div>
             <ul>
-                {!!allProducts &&
-                    allProducts.map((product, index) => (
+                {!!products &&
+                    products.map((product, index) => (
                         <ProductCard product={product} key={index} />
                     ))}
             </ul>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,42 @@
 'use client';
 import HeroCard from '@/app/components/HeroCard';
 import FlowerList from '@/app/components/FlowerList';
-import { useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { GetBouquets } from '@/app/types/products.models';
+import { useToggle } from '@uidotdev/usehooks';
 
 const Home = () => {
+    const [products, setProducts] = useState<GetBouquets | null>(null);
+    const [on, toggle] = useToggle(true);
+
+    const fetchProducts = async () => {
+        const response = await fetch('http://localhost:3000/api/products');
+        const data = await response.json();
+        setProducts(data);
+    };
+
+    useEffect(() => {
+        fetchProducts().catch((error) => {
+            console.error('Error:', error);
+        });
+    }, []);
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/products', {
+            method: 'PUT',
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+            }),
+            body: JSON.stringify({ priceIncludingVat: on }),
+        })
+            .then((response) => response.json())
+            .then((data) => setProducts(data))
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+    }, [on]);
+
     const scrollToSection = useRef<HTMLElement | null>(null);
     const executeScrollToRef = () =>
         scrollToSection?.current?.scrollIntoView({ behavior: 'smooth' });
@@ -14,7 +47,7 @@ const Home = () => {
                 <HeroCard executeScroll={executeScrollToRef} />
             </section>
             <section className='px-4 pt-12 max-md:pt-20' ref={scrollToSection}>
-                <FlowerList />
+                <FlowerList products={products} toggle={toggle} on={on} />
             </section>
         </>
     );
